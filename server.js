047555149d64fa0a5f234a1284a8f9692a1c3ad3
chidalu okechukwu <73858189+Chidalu567@users.noreply.github.com>
2022-11-02 10:59:51 +1100

    Documeting the Apis using swagger, this allows easy access for backend developers working remotely

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const cors = require('cors'); // require cors from nodemodules
 const helmet = require('helmet'); // require helmet libnary from node modules
 const dotenv = require('dotenv');
 const mongoose = require('mongoose');
+const swaggerJsDoc = require("swagger-jsdoc");
+const swaggerUi = require("swagger-ui-express");
 
 
     //configure access to environmental variables
@@ -27,7 +29,7 @@ app.use(express.urlencoded({ extended: false })); //configure bodyparser for eas
 
     //cross-origin-resource sharing configuration in nodejs
 app.use(cors({
-    origin:['https://www.spamfoundation.org'],
+    origin:['https://www.spamfoundation.org',"http://localhost:3000"],
     methods: ['POST','GET'],
     optionsSuccessStatus: 200
 }));
@@ -41,6 +43,33 @@ app.use(helmet.contentSecurityPolicy());
     // external Routers
 app.use('/api', newsletterRouter);
 app.use('/api', checkoutRouter);
+
+    // create swagger configuration
+const options = {
+    definition: { // definition or information about the doc
+        openapi: "3.0.0", //open api version
+        info: { // main information about the docs
+            title: "SirPhilipOrganization Api",
+            version: "1.0.0",
+            description: "THe overal view of all api endpoints present in SirPhilp",
+        },
+        servers: [ // list of all servers to target
+            {
+                url: "http://localhost:5000/api",
+            },
+            {
+                url:"https://sporg.herokuapp.com/api"
+            },
+        ]
+    },
+    apis:["./router/*.js"] // file position of all router configuration
+}
+
+    // create swager docs instance
+const specs = swaggerJsDoc(options);
+
+    // api endpoints for swagger docs
+app.use('/api/swagger-docs', swaggerUi.serve, swaggerUi.setup(specs));
 
     // listen to post
 app.listen(process.env.PORT || 5000, () => {
